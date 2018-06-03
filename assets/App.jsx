8297f71009ee05b35e649be1709f5753ec9bbba1
dogix3 <32942909+dogix3@users.noreply.github.com>
2018-06-03 13:33:27 -0600

    se agrega login

--- assets/App.jsx
+++ assets/App.jsx
@@ -6,7 +6,7 @@ var Nav = Reactstrap.Nav;
 var NavItem = Reactstrap.NavItem;
 var NavLink = Reactstrap.NavLink;
 var UncontrolledDropdown = Reactstrap.UncontrolledDropdown;
-var DropdownToggle =  Reactstrap.DropdownToggle;
+var DropdownToggle = Reactstrap.DropdownToggle;
 var DropdownMenu = Reactstrap.DropdownMenu;
 var DropdownItem = Reactstrap.DropdownItem;
 var Container = Reactstrap.Container;
@@ -14,105 +14,143 @@ var Row = Reactstrap.Row;
 var Col = Reactstrap.Col;
 
 class App extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = { programas: [], programa: [], revisiones: [], revision: []  }
-        this.handleReload = this.handleReload.bind(this);
-        this.handleChangeData = this.handleChangeData.bind(this);
-        this.handleChangePrograma = this.handleChangePrograma.bind(this);
-        this.handleChangeRevision = this.handleChangeRevision.bind(this);
+  constructor(props) {
+    super(props)
+    this.state = { programas: [], programa: [], revisiones: [], revision: [], usuario: [], is_login: false }
+    this.handleReload = this.handleReload.bind(this);
+    this.handleChangeData = this.handleChangeData.bind(this);
+    this.handleChangePrograma = this.handleChangePrograma.bind(this);
+    this.handleChangeRevision = this.handleChangeRevision.bind(this);
+    this.checkLogin = this.checkLogin.bind(this);
+  }
+  handleReload() {
+
+    fetch('./server/index.php/programa')
+
+      .then((response) => {
+
+        return response.json()
+
+      })
+
+      .then((data) => {
+
+        this.setState({ programas: data });
+
+        this.forceUpdate();
+
+      })
+
+
+
+    fetch('./server/index.php/revision')
+
+      .then((response) => {
+
+        return response.json()
+
+      })
+
+      .then((data) => {
+
+        this.setState({ revisiones: data });
+
+        this.forceUpdate();
+
+      })
+
+  }
+  componentWillMount() {
+    this.handleReload();
+  }
+  handleChangeData() {
+    this.handleReload();
+  }
+  handleChangePrograma(data) {
+    this.setState({ programa: data })
+  }
+  handleChangeRevision(data) {
+    this.setState({ revision: data })
+  }
+  checkLogin(usuario_2, password_2) {
+    fetch("./server/index.php/programa/" + this.state.id, {
+
+      headers: {
+        'Content-Type': 'application/json',
+
+        'Content-Length': 200
+      },
+
+      body: JSON.stringify({
+
+        usuario: usuario_2,
+
+        password: password_2,
+
+      })
+
+    }).then((response) => {
+
+      //this.props.handleChangeData();
+
+    }).then((data) => {
+
+      this.setState({ usuario: data });
+
+    })
+
+    if (usuario.usuario == usuario_2 && usuario.usuario == password_2) {
+      this.setState({is_login: true});
     }
-    handleReload() {
-
-         fetch('./server/index.php/programa')
-
-           .then((response) => {
-
-               return response.json()
-
-           })
-
-           .then((data) => {
-
-               this.setState({ programas: data });
-
-               this.forceUpdate();
-
-           })
-
-
-
-           fetch('./server/index.php/revision')
-
-           .then((response) => {
-
-               return response.json()
-
-           })
-
-           .then((data) => {
-
-               this.setState({ revisiones: data });
-
-               this.forceUpdate();
-
-           })
-
-       }
-    componentWillMount() {
-        this.handleReload();
-    }
-    handleChangeData() {
-        this.handleReload();
-    }
-    handleChangePrograma(data) {
-      this.setState({programa: data})
-    }
-    handleChangeRevision(data) {
-      this.setState({revision: data})
-    }
-    render() {
-        return (<div><Navbar color="light" light expand="md">
-          <NavbarBrand href="/">Datos de Facturas</NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="http://programacion-con-reactjs.readthedocs.io">Tutorial</NavLink>
-              </NavItem>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Options
+  }
+  render() {
+    if (this.state.is_login) {
+      return (<div><Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Datos de Facturas</NavbarBrand>
+        <NavbarToggler onClick={this.toggle} />
+        <Collapse isOpen={this.state.isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="http://programacion-con-reactjs.readthedocs.io">Tutorial</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
                 </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>
-                    Option 1
+              <DropdownMenu right>
+                <DropdownItem>
+                  Option 1
                   </DropdownItem>
-                  <DropdownItem>
-                    Option 2
+                <DropdownItem>
+                  Option 2
                   </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>
-                    Reset
+                <DropdownItem divider />
+                <DropdownItem>
+                  Reset
                   </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-            </Nav>
-          </Collapse>
-        </Navbar><Container><Row>
-        <Col xs="8"><ProgramasList programas={this.state.programas} 
-                 handleChangePrograma={this.handleChangePrograma}/></Col>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+        </Collapse>
+      </Navbar><Container><Row>
+        <Col xs="8"><ProgramasList programas={this.state.programas}
+          handleChangePrograma={this.handleChangePrograma} /></Col>
         <Col xs="4"><ProgramasForm programa={this.state.programa}
-                 handleChangeData={this.handleChangeData}/></Col>
-        </Row>
-        <Row>
-        <Col xs="8"><RevisionesList revisiones={this.state.revisiones} 
-                programa={this.state.programa}
-                 handleChangeRevision={this.handleChangeRevision}/></Col>
-        <Col xs="4"><RevisionesForm revision={this.state.revision}
-                programa={this.state.programa}
-                 handleChangeData={this.handleChangeData}/></Col>
-        </Row></Container></div>)
+          handleChangeData={this.handleChangeData} /></Col>
+      </Row>
+          <Row>
+            <Col xs="8"><RevisionesList revisiones={this.state.revisiones}
+              programa={this.state.programa}
+              handleChangeRevision={this.handleChangeRevision} /></Col>
+            <Col xs="4"><RevisionesForm revision={this.state.revision}
+              programa={this.state.programa}
+              handleChangeData={this.handleChangeData} /></Col>
+          </Row></Container></div>)
+    } else {
+      return (
+        <LoginForm checkLogin={this.checkLogin} />
+      )
     }
+  }
 }
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ assets/LoginForm.jsx
@@ -0,0 +1,76 @@
+var Form = Reactstrap.Form;
+var Button = Reactstrap.Button;
+var FormGroup = Reactstrap.FormGroup;
+var Label = Reactstrap.Label;
+var Input = Reactstrap.Input;
+var Row = Reactstrap.Row;
+var Col = Reactstrap.Col;
+
+class LoginForm extends React.Component {
+
+    constructor(props) {
+
+        super(props)
+
+        this.state = { id: "", is_login: "", usuario: "", password: "", nombre: "", tipo_usuario: "" }
+
+        this.login = this.login.bind(this);
+
+    }
+
+    handleFields(event) {
+
+        const target = event.target;
+
+        let value = target.value;
+
+        const name = target.name;
+
+        this.setState({ [name]: value });
+
+    }
+    login() {
+        this.props.checkLogin(this.state.usuario, this.state.password);
+    }
+    render() {
+
+        return (<Form>
+            <Container>
+                <Row>
+                    <div className="text-center divLoginContent">
+                        <Col xs="4" className="frmLoginContent">
+                            <Row>
+                                <Col xs="12">
+                                    <Label>Usuario</Label>
+                                </Col>
+                                <Col xs="12">
+                                    <Input type="text" name="usuario"
+                                        value={this.state.usuario} onChange={this.handleFields} />
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col xs="12">
+                                    <Label>Contraseña</Label>
+                                </Col>
+                                <Col xs="12">
+                                    <Input type="password" name="password"
+                                        value={this.state.password} onChange={this.handleFields} />
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col xs="12">
+                                    <Button className="btn_login" onClick={this.login}>Ingresar</Button>
+                                </Col>
+                                <Col xs="12">
+                                    <Button>Registrarse</Button>
+                                </Col>
+                            </Row>
+                        </Col>
+                    </div>
+                </Row>
+            </Container>
+        </Form>)
+
+    }
+
+}
